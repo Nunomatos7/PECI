568c9395aac776dcbf8c18b5a09daec88e64d095
Joao rodrigues <joaopprodrigues08@ua.pt>
2022-11-29 16:07:07 +0000

    ligação à base de dados

--- projeto/personal/views.py
+++ projeto/personal/views.py
@@ -1,9 +1,14 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+#import pymysql
+from models import Desovas
 from django.shortcuts import render
 
 # Create your views here.
 
 def home_screen_view(request):
     return render(request, "base.html", {})
+
+def send(request):
+    p = Desovas(request[0],request[1],request[2],request[3])
+    p.save()
