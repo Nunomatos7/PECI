365a33359ee9786cdca0f6eca3724aacb2c5e468
Joao rodrigues <joaopprodrigues08@ua.pt>
2022-12-21 02:23:40 +0000

    models and form

--- app/personal/models.py
+++ app/personal/models.py
@@ -3,4 +3,36 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Temperatura (models.Model):
+    data = models.DateField
+    temperatura = models.FloatField
+
+class Desova (models.Model):
+    data = models.DateField
+    femeas = models.IntegerField
+    desovados = models.IntegerField
+    embrionados = models.IntegerField
+
+class Jaula (models.Model):
+    id = models.PositiveSmallIntegerField
+    num_peixes = models.PositiveIntegerField
+    data = models.DateField
+    PM = models.FloatField
+    Blom = models.FloatField
+    percentagem_alimentacao = models.FloatField
+    peso = models.FloatField
+    sacos_racao = models.FloatField
+    FC = models.FloatField
+    peso_medio = models.FloatField
+    alimentacao_teorica = models.FloatField
+    alimentacao_real = models.FloatField
+    PM_teorico = models.FloatField
+    PM_real = models.FloatField
+    percentagem_mortalidade_teorica = models.FloatField
+    num_mortos_teorico = models.FloatField
+    percentagem_mortalidade_real = models.FloatField
+    num_mortos_real = models.FloatField
+    retirados = models.PositiveSmallIntegerField
+    colocados = models.PositiveSmallIntegerField
+    FC_real = models.FloatField
+
--- app/personal/views.py
+++ app/personal/views.py
@@ -2,8 +2,11 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
-
+temperaturas = Temperatura.objects.all
+print(temperaturas)
 def home_screen_view(request):
-    return render(request, "base.html", {})
+    return render(request, "base.html", {'temperaturas' : temperaturas})
+
+
